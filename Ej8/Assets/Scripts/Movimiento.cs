8288eb54eb3328693b777b5a114402f7845dafe6
JuanFranCrater <juanfbenitezlopez@gmail.com>
2017-10-23 17:20:14 +0200

    Ejercicios 1,2,3 terminados, 8 empezado

---
+++ Ej8/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour {
+
+	float _xpos = 0.0F;
+	float _zpos = 0.0F;
+	float _velocidad=1F;
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		_xpos = Input.GetAxis ("Horizontal") * Time.deltaTime * _velocidad;
+
+		_zpos = Input.GetAxis ("Vertical") * Time.deltaTime * _velocidad;
+		transform.Translate (_xpos, 0, _zpos);
+
+		Vector3 positionClamp = transform.position;
+
+		positionClamp.z = Mathf.Clamp (transform.position.z, -14.5F, 14.5F);
+		positionClamp.x = Mathf.Clamp (transform.position.x, -9.5F, 9.5F);
+
+		transform.position = positionClamp;
+		transform.rotation = new Quaternion(0,0,0,0);
+	}
+}
