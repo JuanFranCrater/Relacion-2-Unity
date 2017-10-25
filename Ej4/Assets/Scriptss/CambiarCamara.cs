1147f145256ec1eeeb13d0891567cc306147217e
JuanFranCrater <juanfbenitezlopez@gmail.com>
2017-10-25 18:12:57 +0200

    Ejercicios alterados y Ej6 hecho

--- Ej4/Assets/Scriptss/CambiarCamara.cs
+++ Ej4/Assets/Scriptss/CambiarCamara.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CambiarCamara : MonoBehaviour {
-
+	Camera[] camaras= new Camera[2];
+	bool camara=true;
 	// Use this for initialization
 	void Start () {
-		
+		Camera.GetAllCameras(camaras);
+		camaras [1].enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if (camara) {
+				camaras [0].enabled = false;
+				camaras [1].enabled = true;
+			
+				camara = false;
+			} else {
+				camaras [1].enabled = false;
+				camaras [0].enabled = true;
+				camara = true;
+			}
+		}
 	}
 }
